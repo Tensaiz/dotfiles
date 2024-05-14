dd84a48b5e2e3d8212e24b20fe784301b9567797
Tensaiz <matevy1@gmail.com>
2024-05-14 15:12:37 +0000

    Reorganize folder structure

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+set fish_greeting
+cat ~/.cache/wal/sequences &
 
+zoxide init --cmd cd fish | source
 starship init fish | source
